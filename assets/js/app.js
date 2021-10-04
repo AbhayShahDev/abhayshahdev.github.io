95309886fa1a13aa4261f48cd19aa7ae1eebc644
Abhay <abhayshahdev@gmail.com>
2021-10-05 01:13:22 +0530

    Added some elements with styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,12 +14,128 @@ if(navClose){
 }
 
 //Remove Menu Mobile
-
-// var ul = document.getElementsByClassName("nav-list");
 var items = document.getElementsByClassName("nav-link");
 for (var i = 0; i < items.length; ++i) {
-  // do something with items[i], which is a <li> element
   items[i].addEventListener('click', ()=>{
     navMenu.classList.remove('show-menu');
   })
 }
+
+//Accordions
+const skillsContent = document.getElementsByClassName("skills-content"),
+      skillsHeader = document.querySelectorAll(".skills-header"),
+      skillsList = document.getElementById("skills-list");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for(i=0; i < skillsContent.length; i++) {
+      skillsContent[i].className = 'skills-content skills-close';
+  }
+  if(itemClass === 'skills-content skills-close') {
+    this.parentNode.className = 'skills-content skills-open';
+  }
+}
+
+skillsHeader.forEach(elem => {
+  elem.addEventListener('click', toggleSkills)
+});
+
+// Modals
+const modalViews = document.querySelectorAll('.services-modal'),
+      modalBtns = document.querySelectorAll('.services-button'),
+      modalCloses = document.querySelectorAll('.services-modal-close');
+
+let modal = function(modalClick){
+  modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(i);
+  })
+})
+
+modalCloses.forEach((modalClose) =>{
+  modalClose.addEventListener('click', () =>{
+    modalViews.forEach((modalView) =>{
+      modalView.classList.remove('active-modal');
+    })
+  })
+});
+
+//Portfolio
+
+filterSelection("all")
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("portfolio-column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    portfolioRemoveClass(x[i], "portfolio-show");
+    if (x[i].className.indexOf(c) > -1) portfolioAddClass(x[i], "portfolio-show");
+  }
+}
+
+function portfolioAddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function portfolioRemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("portfolio-btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// Dark/Light Theme
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'fa-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
